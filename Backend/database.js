4cb5fe6eacdadcd24e63c0370f9022ce461e19f1
bhavnapandit <pritybhavna2001@gmail.com>
2025-05-23 12:21:52 +0530

    add

--- Backend/database.js
+++ Backend/database.js
@@ -1,16 +1,15 @@
-// database.js
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-const connection = await mysql.createConnection({
+const db = await mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
-
-console.log('Connected to MySQL DB');
-
-export default connection;
+export default db;
