83f1b92bf4131413d9af691313e5d4d936d75a25
bhavnapandit <pritybhavna2001@gmail.com>
2025-05-23 12:53:40 +0530

    add

--- Backend/database.js
+++ Backend/database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const db = await mysql.createPool({
+export const connection = await mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
